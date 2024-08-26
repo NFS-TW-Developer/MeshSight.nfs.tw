4c319bd9e909cf980659b101c2d15a27c0ed7b8a
edwinyoo44 <7691822+edwinyoo44@users.noreply.github.com>
2024-08-26 22:51:37 +0800

    Release 0.4.0+240826231

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"235db911ba279722f5e685f38b0ed30fa7e857
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "829466189"
+    serviceWorkerVersion: "3688272259"
   }
 });
