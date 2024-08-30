8c051d68cc40d7d5e43cc7a3f0fa8eeb7be3992d
edwinyoo44 <7691822+edwinyoo44@users.noreply.github.com>
2024-08-30 23:33:37 +0800

    Release 0.6.0+240830317

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -3,11 +3,11 @@ const MANIFEST = 'flutter-app-manifest';
 const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 
-const RESOURCES = {"flutter_bootstrap.js": "9aa5c17427fe22d3bb1a9d872fe50a47",
-"version.json": "37877f67c429789499a565bc4c7cf30b",
+const RESOURCES = {"flutter_bootstrap.js": "700501482832c6b76588dca8b55e8cfd",
+"version.json": "1d9b80cf5b799918547f4ecbc45aef63",
 "index.html": "7511c10d41ba7c5d55c3e3daf4e1cded",
 "/": "7511c10d41ba7c5d55c3e3daf4e1cded",
-"main.dart.js": "02dc1036c9832b69a2fc3033f486008e",
+"main.dart.js": "45e2cf96202f976bec09b2621cf5c11f",
 "flutter.js": "383e55f7f3cce5be08fcf1f3881f585c",
 "favicon.png": "80ca9cbe76afd413e54645871a397f74",
 "icons/Icon-192.png": "109a3f682ed1e8ddd0137a49f807c404",
